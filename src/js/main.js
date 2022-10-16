c21a44c4dfbe2a2bebe8ce3f23744f369d527007
Anton <spinsib@gmail.com>
2022-10-16 15:38:38 +0700

    add masks, forms, validations in inputs

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,20 @@
 import modals from "./modules/modals";
+import sliders from "./modules/sliders";
+import forms from "./modules/forms";
+import mask from "./modules/mask";
+import checkNumInputs from "./modules/checkTextInputs";
+import showMoreStyles from "./modules/showMoreStyles";
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
     modals();
-    
+    sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
+    sliders('.main-slider-item', 'vertical');
+    forms();
+    mask('[name="phone"]');
+    checkNumInputs('[name="name"]');
+    checkNumInputs('[name="message"]');
+    showMoreStyles('.button-styles', '.styles-2');
+
 });
